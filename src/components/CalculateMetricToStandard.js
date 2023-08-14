7e0b8cd8b030c0c6d53c4efae644cd7fd97702ae
ericbowser <ericryanbowser@gmail.com>
2023-08-13 22:54:55 -0600

    start adding calculations

--- src/components/CalculateMetricToStandard.js
+++ src/components/CalculateMetricToStandard.js
@@ -1,10 +1,8 @@
 ﻿
 import React, { useState, useEffect } from 'react';
-import Tab from 'react-bootstrap/Tab';
-import Tabs from 'react-bootstrap/Tabs';
 import Container from 'react-bootstrap/Container';
 import Form from 'react-bootstrap/Form';
-import Button from 'react-bootstrap/Button';
+import OutlineButton from 'react-bootstrap/Button';
 import styled from 'styled-components';
 import Alert from 'react-bootstrap/Alert';
 import Dropdown from 'react-bootstrap/Dropdown';
@@ -25,12 +23,16 @@ const RightColumn = styled.div`
   grid-column-end: -1;
   grid-row: 1;
 `
-const TabsContainer = styled(Tabs)`
-`
-const ButtonContainer = styled(Button)`
-`
-
-
+/*
+const Formula = {
+    MillimetersToCentimeters,
+    MillimetersToInches,
+    MillimetersToFeet,
+    CentimetersToMillimeters,
+    InchesToMillimeters,
+    FeetToMillimeters
+}
+*/
 
 function MetricToStandardPage() {
     
@@ -39,82 +41,111 @@ function MetricToStandardPage() {
     const [ calcValue, setCalcValue ] = useState(null);
     
     useEffect(() => {
-        console.log('from value')
     }, [fromValue]);
     
     useEffect(() => {
-        console.log('to value')
     }, [toValue]);
     
     useEffect(() => {
     }, [calcValue]);
 
-    function calculate() {
-        console.log(fromValue);
-        console.log(toValue);
-        const calcValue = fromValue + toValue;
-        setCalcValue(calcValue);
+    function decideCalculation(inputValue) {
+        let value;
+        if (fromValue === 'millimeters' && toValue === 'centimeters') {
+            value = inputValue / 10;
+            return value;
+        }
+        else if (fromValue === 'centimeters' && toValue === 'millimeters') {
+            value = inputValue * 10;
+            return value;
+        }
+        else if (fromValue === 'millimeters' && toValue === 'inches') {
+            value = inputValue * 25.4;
+            return value;
+        }
+        else if (fromValue === 'inches' && toValue === 'millimeters') {
+            value = inputValue / 25.4;
+            return value;
+        }
+        else if (fromValue === 'millimeters' && toValue === 'feet') {
+            value = inputValue * 304.8;
+            return value;
+        }
+        else if (fromValue === 'feet' && toValue === 'millimeters') {
+            value = inputValue / 304.8;
+            return value;
+        }
+    }
+    
+    function calculate(event) {
+        event.preventDefault();
+        const inputValue = event.target[0].value;
+        
+        if (fromValue && toValue) {
+            const calcValue = decideCalculation(inputValue);
+            setCalcValue(calcValue);
+        }
     }
     
     function onSelectFrom(fromValue) {
-        if (fromValue) {
-            console.log(fromValue);
-            setFromValue(fromValue);
-        }
-        else {
-            setFromValue(null)
-        }
+        setFromValue(fromValue);
     }
     function onSelectTo(toValue) {
-        if (toValue) {
-            console.log(toValue);
-            setToValue(toValue);
-        }
-        else {
-            setToValue(null)
-        }
+        setToValue(toValue);
     }
    
     return (
         <React.Fragment>
+            <Container style={{padding: '25px'}}>
+            <h3>Conversion Units</h3>
+            <div style={{backgroundColor: 'pink', padding: '10px'}}></div>
             <Grid>
-                <Dropdown onSelect={onSelectFrom} key={`From-${fromValue}`}>
-                    <Dropdown.Toggle variant="success" id="dropdown-basic">
-                        From 
-                    </Dropdown.Toggle>
-                    <Dropdown.Menu>
-                        <Dropdown.Item eventKey={"millimeters"} active={fromValue === 'millimeters'}>Millimeters</Dropdown.Item>
-                        <Dropdown.Item eventKey={"centimeters"} active={fromValue === 'centimeters'}>Centimeters</Dropdown.Item>
-                        <Dropdown.Item eventKey={"inches"} active={fromValue === 'inches'}>Inches</Dropdown.Item>
-                    </Dropdown.Menu>
-                </Dropdown>
-                <Dropdown onSelect={onSelectTo} key={`To-${toValue}`}>
-                    <Dropdown.Toggle variant="success" id="dropdown-basic">
-                        To
-                    </Dropdown.Toggle>
-                    <Dropdown.Menu>
-                        <Dropdown.Item eventKey={"millimeters"} active={toValue === 'millimeters'}>Millimeters</Dropdown.Item>
-                        <Dropdown.Item eventKey={"centimeters"} active={toValue === 'centimeters'}>Centimeters</Dropdown.Item>
-                        <Dropdown.Item eventKey={"inches"} active={toValue === 'inches'}>Inches</Dropdown.Item>
-                    </Dropdown.Menu>
-                </Dropdown>
-                <Form onSubmit={calculate}>
-                    <Form.Label htmlFor="input"></Form.Label>
-                    <Form.Text id="input" muted>
-                        Input Value
-                    </Form.Text>
-                    <Form.Control
-                        type="text"
-                        id="input"
-                        aria-describedby="metricToStandard"
-                    >
-                    </Form.Control>
-                    <ButtonContainer type='submit'>Calculate</ButtonContainer>
-                </Form>
-                {calcValue && (
-                    <div>{calcValue}</div>
-                )} 
-            </Grid>
+                    <Dropdown onSelect={onSelectFrom} key={`From-${fromValue}`}>
+                        <div>From Units</div>
+                        <Dropdown.Toggle variant='outline-primary' id="dropdown-basic">
+                            {fromValue ?? 'From'} 
+                        </Dropdown.Toggle>
+                        <Dropdown.Menu>
+                            <Dropdown.Item eventKey={"millimeters"} active={fromValue === 'millimeters'}>Millimeters</Dropdown.Item>
+                            <Dropdown.Item eventKey={"centimeters"} active={fromValue === 'centimeters'}>Centimeters</Dropdown.Item>
+                            <Dropdown.Item eventKey={"inches"} active={fromValue === 'inches'}>Inches</Dropdown.Item>
+                            <Dropdown.Item eventKey={"feet"} active={fromValue === 'feet'}>Feet</Dropdown.Item>
+                        </Dropdown.Menu>
+                    </Dropdown>
+                    <Dropdown  onSelect={onSelectTo} key={`To-${toValue}`}>
+                        <div>To Units</div>
+                        <Dropdown.Toggle variant='outline-primary' id="dropdown-basic">
+                            {toValue ?? 'To'}
+                        </Dropdown.Toggle>
+                        <Dropdown.Menu>
+                            <Dropdown.Item eventKey={"millimeters"} active={toValue === 'millimeters'}>Millimeters</Dropdown.Item>
+                            <Dropdown.Item eventKey={"centimeters"} active={toValue === 'centimeters'}>Centimeters</Dropdown.Item>
+                            <Dropdown.Item eventKey={"inches"} active={toValue === 'inches'}>Inches</Dropdown.Item>
+                            <Dropdown.Item eventKey={"feet"} active={toValue === 'feet'}>Feet</Dropdown.Item>
+                        </Dropdown.Menu>
+                    </Dropdown>
+                    <Form onSubmit={calculate}>
+                        <Form.Label htmlFor="input"></Form.Label>
+                        <Form.Text id="input" muted>
+                            Input Value
+                        </Form.Text>
+                        <Form.Control
+                            type="text"
+                            id="input"
+                            aria-describedby="metricToStandard"
+                        >
+                        </Form.Control>
+                        <OutlineButton variant="outline-dark" style={{marginTop: '15px'}} type='submit'>Calculate</OutlineButton>
+                    </Form>
+                    {calcValue && (
+                        <React.Fragment>
+                            <Alert>{calcValue}</Alert>
+                            <span>{toValue}</span>
+                        </React.Fragment>
+                    )} 
+                </Grid>
+                <div style={{backgroundColor: 'pink', padding: '10px'}}></div>
+            </Container>
         </React.Fragment>
     )
 }
